67c9d71a3bca2892dac17e5ea1a1a78f7663693f
boring_student <boring_student@qq.com>
2025-03-23 18:51:25 +0800

    Update pipv.py

--- pipv.py
+++ pipv.py
@@ -11,7 +11,5 @@
 # pip uninstall "PyQt6-Fluent-Widgets[full]"
 
 # 镜像源
-# 清华大学 https://pypi.tuna.tsinghua.edu.cn/simple/
 # 阿里云 http://mirrors.aliyun.com/pypi/simple/
-# 中国科技大学 https://pypi.mirrors.ustc.edu.cn/simple/
-# 中国科学技术大学 http://pypi.mirrors.ustc.edu.cn/simple/
+
